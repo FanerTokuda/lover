77aefea1311460cc65c87f5a8035964b2034fa65
FanerTokuda <86337345+FanerTokuda@users.noreply.github.com>
2022-07-17 10:53:25 +0700

    Add files via upload
    create

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = '18-03-2003';
+const giftUrl = 'http://google.com';
+const eventName = 'Gửi tặng em';
+const titleCard = 'CÔ GÁI HỌ ĐẶNG';
+const contentCard = 'Chúc Châu của anh tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+const giftImage = 'hot-girl.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
+
