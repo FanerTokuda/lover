1d57e6e2cdcfa9b7433071c0ab9445c74aaca4ab
FanerTokuda <86337345+FanerTokuda@users.noreply.github.com>
2022-07-17 11:09:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = '18-03-2003';
 const giftUrl = 'https://drive.google.com/file/d/1hfh_7KXVcwm7iVl5_SaNJyevvKyPFrLO/view?usp=sharing';
 const eventName = 'Gửi tặng em';
 const titleCard = 'CÔ GÁI HỌ ĐẶNG';
-const contentCard = 'Chúc Châu của anh tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const contentCard = '❤ Chúc Châu của anh tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau. Chúc em nhiều sức khỏe, sức mạnh để hoàn thành tốt mọi việc. Yêu em ❤';
 
 const giftImage = 'hot-girl.jpg';
 const base64 = '';
